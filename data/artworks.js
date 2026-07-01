e47628a7a502782a8fb35b5fb83ce1aff84892f0
garsonnoa <garsonnoa85@gmail.com>
2026-07-01 09:30:43 +0100

    Create artworks.js

---
+++ data/artworks.js
@@ -0,0 +1,52 @@
+const ARTWORKS = [
+    {
+        title: "The Starry Night",
+        artist: "Vincent van Gogh",
+        image: "images/starry_night.jpg"
+    },
+    {
+        title: "Mona Lisa",
+        artist: "Leonardo da Vinci",
+        image: "images/mona_lisa.jpg"
+    },
+    {
+        title: "The Persistence of Memory",
+        artist: "Salvador Dalí",
+        image: "images/persistence_of_memory.jpg"
+    },
+    {
+        title: "The Scream",
+        artist: "Edvard Munch",
+        image: "images/the_scream.jpg"
+    },
+    {
+        title: "Girl with a Pearl Earring",
+        artist: "Johannes Vermeer",
+        image: "images/girl_with_pearl_earring.jpg"
+    },
+    {
+        title: "The Night Watch",
+        artist: "Rembrandt van Rijn",
+        image: "images/the_night_watch.jpg"
+    },
+    {
+        title: "Guernica",
+        artist: "Pablo Picasso",
+        image: "images/guernica.jpg"
+    },
+    {
+        title: "The Kiss",
+        artist: "Gustav Klimt",
+        image: "images/the_kiss.jpg"
+    },
+    {
+        title: "American Gothic",
+        artist: "Grant Wood",
+        image: "images/american_gothic.jpg"
+    },
+    {
+        title: "The Birth of Venus",
+        artist: "Sandro Botticelli",
+        image: "images/birth_of_venus.jpg"
+    }
+];
